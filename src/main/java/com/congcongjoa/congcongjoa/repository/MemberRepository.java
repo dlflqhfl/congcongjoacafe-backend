1f808193f12f359817d3070bc2d9ba243ddc3379
‘Haun’ <hawoon_143@naver.com>
2024-11-19 23:57:39 +0900

    CORS 설정

--- src/main/java/com/congcongjoa/congcongjoa/repository/MemberRepository.java
+++ src/main/java/com/congcongjoa/congcongjoa/repository/MemberRepository.java
@@ -1,10 +1,16 @@
 package com.congcongjoa.congcongjoa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.congcongjoa.congcongjoa.entity.Member;
+import com.congcongjoa.congcongjoa.entity.enumurate.BooleanStatus;
 import com.congcongjoa.congcongjoa.repository.custom.MemberRepositoryCustom;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
-    
+    @Query("select count(m) > 0 from Member m where m.mEmail = :mEmail and m.mStatus = :mStatus")
+    boolean existsByMEmailAndMStatus(String mEmail, BooleanStatus mStatus);
+
 }
--- src/main/java/com/congcongjoa/congcongjoa/service/MemberService.java
+++ src/main/java/com/congcongjoa/congcongjoa/service/MemberService.java
@@ -3,6 +3,7 @@ package com.congcongjoa.congcongjoa.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.congcongjoa.congcongjoa.entity.enumurate.BooleanStatus;
 import com.congcongjoa.congcongjoa.repository.MemberRepository;
 
 @Service
@@ -10,5 +11,11 @@ public class MemberService {
 
     @Autowired
     private MemberRepository memberRepository;
+
+    public boolean isEmailDuplicate(String email){
+        boolean isDuplicate = memberRepository.existsByMEmailAndMStatus(email, BooleanStatus.TRUE);
+
+        return isDuplicate;
+    }
     
 }
