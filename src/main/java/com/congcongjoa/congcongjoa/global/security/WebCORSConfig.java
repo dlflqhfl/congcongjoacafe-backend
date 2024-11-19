618dcf8c9c232638fb8d564bee5cd4fefa36e0a0
‘Haun’ <hawoon_143@naver.com>
2024-11-19 23:57:39 +0900

    CORS 설정

--- src/main/java/com/congcongjoa/congcongjoa/global/security/WebCORSConfig.java
+++ src/main/java/com/congcongjoa/congcongjoa/global/security/WebCORSConfig.java
@@ -10,7 +10,7 @@ public class WebCORSConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:9090", "http://43.200.245.38")
+                .allowedOriginPatterns("http://localhost:9090", "http://43.200.245.38", "http://localhost:3000")
                 .allowedHeaders("*")
                 .allowedMethods("*")
                 .allowCredentials(true);
