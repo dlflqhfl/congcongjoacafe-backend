e42e1462dc22b135a638603f80e260952c1912cb
Seungho <dlflqhfl@gmail.com>
2024-11-23 19:27:39 +0900

    jwt로그인 구현 및 보안 설정

--- src/main/java/com/congcongjoa/congcongjoa/service/StoreService.java
+++ src/main/java/com/congcongjoa/congcongjoa/service/StoreService.java
@@ -1,11 +1,8 @@
 package com.congcongjoa.congcongjoa.service;
 
 import com.congcongjoa.congcongjoa.dto.StoreDTO;
-import com.congcongjoa.congcongjoa.dto.custom.CustomOwnerDetails;
 import com.congcongjoa.congcongjoa.entity.Store;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.congcongjoa.congcongjoa.dto.custom.RegStoreDTO;
 import com.congcongjoa.congcongjoa.enums.StoreStatus;
